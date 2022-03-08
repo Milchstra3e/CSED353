76484639a5436d9c4d06feeb15f6013a5af358b9
Changhun Oh <milchstra3e@gmail.com>
2022-03-08 00:47:05 -0800

    edit stream_reassembler - add attribute and function in StreamReassembler - implement each functions

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,26 +1,54 @@
 #include "stream_reassembler.hh"
 
-// Dummy implementation of a stream reassembler.
-
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
-
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+#include <iostream>
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity),
+    _unassm_base(0), _unassm_size(0), _eof(false), _buffer(capacity, 0), _buffer_check(capacity, false) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    const size_t data_begin = max(index, _unassm_base);
+    const size_t data_expect = max(index + data.length(), _unassm_base);
+    const size_t data_end = min(data_expect, _get_upperbound());
+
+    // push data into buffer
+    _eof |= (data_expect == data_end) && eof;
+    for(size_t i = data_begin; i < data_end; i++) {
+        const size_t data_idx = i - index;
+        const size_t base_idx = i - _unassm_base;
+
+        if(!_buffer_check[base_idx]) {
+            _buffer[base_idx] = data[data_idx];
+            _buffer_check[base_idx] = true;
+            _unassm_size++;
+        }
+    }
+
+    // move data from buffer to bytestream
+    string concat_data;
+    for(size_t i = 0; i < _capacity && _buffer_check.front(); i++) {
+        concat_data += _buffer.front();
+        _unassm_base++;
+        _unassm_size--;
+        _buffer.pop_front();
+        _buffer_check.pop_front();
+    }
+    _output.write(concat_data);
+    if(_unassm_size == 0 && _eof) _output.end_input();
+
+    // restore total buffer size
+    _buffer.resize(_capacity, 0);
+    _buffer_check.resize(_capacity, false);
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _unassm_size; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _unassm_size == 0; }
+
+size_t StreamReassembler::_get_upperbound() const {
+    return _capacity + _unassm_base - _output.buffer_size();
+}
