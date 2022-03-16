26535e29788d2c9b485fcf8662df14127af30eab
Changhun Oh <milchstra3e@gmail.com>
2022-03-16 03:00:53 -0700

    edit wrapping_integer.cc - edit function: wrap, unwrap - remove unnecessary things

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,22 +1,11 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
-
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32{isn + n}; }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +18,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t relative_seqno = static_cast<uint32_t>(n - isn);
+
+    if (checkpoint <= relative_seqno)
+        return relative_seqno;
+
+    const uint64_t ring_cnt = (checkpoint - relative_seqno) >> 32;
+    const uint64_t lower_bound = relative_seqno + (ring_cnt << 32);
+    const uint64_t upper_bound = relative_seqno + ((ring_cnt + 1) << 32);
+    return (checkpoint - lower_bound) > (upper_bound - checkpoint) ? upper_bound : lower_bound;
 }
