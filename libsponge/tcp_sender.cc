4f125a167ec0c2d581105e57daa249e6c367fb0d
Changhun Oh <milchstra3e@gmail.com>
2022-03-30 19:22:05 -0700

    edit functions

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -75,20 +75,91 @@ TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const s
     , _stream(capacity)
     , _retx_manager(retx_timeout) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return _next_seqno - _curr_ackno; }
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+    const bool syn = (_next_seqno == 0);
+    const bool fin = (_stream.input_ended() && !_eof);
+    const uint16_t required_window_size = syn + _stream.buffer_size() + fin;
+
+    const bool has_fin = _current_window_size >= required_window_size;
+    const uint16_t total_payload_size = min(_current_window_size, required_window_size) - syn - (fin && has_fin);
+
+    const unsigned int segment_cnt =
+        total_payload_size / TCPConfig::MAX_PAYLOAD_SIZE + (total_payload_size % TCPConfig::MAX_PAYLOAD_SIZE > 0);
+
+    for (unsigned int i = 0; i < segment_cnt; i++) {
+        const uint16_t remain_payload_size = total_payload_size - i * TCPConfig::MAX_PAYLOAD_SIZE;
+        const uint16_t read_bytes = min(static_cast<size_t>(remain_payload_size), TCPConfig::MAX_PAYLOAD_SIZE);
+
+        const string payload = _stream.read(read_bytes);
+
+        const bool is_first_iteration = (i == 0);
+        const bool is_last_iteration = (i + 1 == segment_cnt);
+
+        const TCPSegment segment =
+            _gen_segment(_isn + _next_seqno, syn && is_first_iteration, has_fin && fin && is_last_iteration, payload);
+
+        _segments_out.push(segment);
+        _retx_manager.push_segment(_gen_absolute_seqno(_isn + _next_seqno), segment);
+
+        _next_seqno += segment.length_in_sequence_space();
+        _current_window_size -= segment.length_in_sequence_space();
+        _eof |= has_fin && fin && is_last_iteration;
+    }
+
+    if (_current_window_size) {
+        if (syn) {
+            const TCPSegment segment = _gen_segment(_isn, true, false, string());
+            _next_seqno++;
+            _current_window_size--;
+
+            _segments_out.push(segment);
+            _retx_manager.push_segment(_gen_absolute_seqno(_isn), segment);
+        }
+
+        if (fin && !_eof) {
+            const TCPSegment segment = _gen_segment(_isn + _next_seqno, false, true, string());
+            _next_seqno++;
+            _current_window_size--;
+            _eof |= fin;
+
+            _segments_out.push(segment);
+            _retx_manager.push_segment(_gen_absolute_seqno(_isn + _next_seqno), segment);
+        }
+    }
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    const uint64_t tmp_curr_ackno = _gen_absolute_seqno(ackno);
+
+    if (tmp_curr_ackno <= _next_seqno) {
+        _curr_ackno = max(_curr_ackno, tmp_curr_ackno);
+        _receiver_window_size = window_size;
+
+        const uint16_t tmp_window_size = max(window_size, uint16_t(1));
+
+        _current_window_size = max(uint64_t(0), tmp_window_size - (_next_seqno - _curr_ackno));
+        _retx_manager.update(_curr_ackno);
+    }
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    const optional<TCPSegment> retx_segment = _retx_manager.alarm_check(ms_since_last_tick, _receiver_window_size);
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+    if (retx_segment.has_value())
+        _segments_out.push(retx_segment.value());
+}
 
-void TCPSender::send_empty_segment() {}
+unsigned int TCPSender::consecutive_retransmissions() const { return _retx_manager.get_consecutive_retx(); }
+
+void TCPSender::send_empty_segment() {
+    const TCPSegment segment = _gen_segment(wrap(_next_seqno, _isn), false, false, string());
+    _segments_out.push(segment);    
+}
 
 TCPSegment TCPSender::_gen_segment(WrappingInt32 seqno, bool syn, bool fin, string payload) {
     TCPSegment segment;
